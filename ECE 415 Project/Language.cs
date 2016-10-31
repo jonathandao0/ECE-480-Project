39a753b3a63af51649979ea953b3753ed3796e40
jonathandao0 <jonathandao0@gmail.com>
2016-10-31 09:08:38 -0700

    Initial Commit

---
+++ ECE 415 Project/Language.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECE_415_Project
+{
+    class Language
+    {
+        public double runtime { get; set; }
+        public double probability { get; set; }
+        public int rank { get; set; }   // Is it necessary?
+        public string language { get; set; }
+        public string input { get; set; }
+    }
+}
