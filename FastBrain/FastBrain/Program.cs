1ea467a570162c161e33706e1ce26ac0b8744a59
Kevin <kevincrisanto@gmail.com>
2016-11-09 00:35:25 -0800

    Fast learning program

---
+++ FastBrain/FastBrain/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables
+            double percentage, wordsFound = 0;
+            string line;
+            // This array contains the most common words in English
+            string[] English = { "ABOUT", "ALL", "AN", "AND", "AS", "AT", "BE", "BUT", "CAN", "DAY", "DO", "DOG", "EXAMPLE", "FOR", "FROM",
+                "GET", "GIVE", "GO",
+                "HAPPY", "HAVE", "HE", "HER", "I", "IF", "IN", "IS", "LIKE",
+                "MAKE", "ME", "MINE", "MOST", "NOT", "OF", "ON", "ONE", "OR", "OUT", "SAY", "SHE", "SO",
+                "TEST", "THAT", "THE", "THEIR", "THERE", "THEY", "THIS", "TIME", "TO", "UP", "US",
+                "WE", "WHEN", "WHERE", "WHICH", "WHO", "WILL", "WITH", "WOULD", "YOU" };
+
+            // Allow user to type string
+            Console.Write("Enter text: ");
+            line = Console.ReadLine().ToUpper();
+            string[] words = line.Split(' ');
+
+            // Check if letters belong to English alphabet
+            if (Regex.IsMatch(line, "^[a-zA-Z ]+$"))
+            {
+                // Check number of words in English
+                Console.WriteLine("All characters belong to the English alphabet");
+                for (int i = 0; i < words.Length; i++)
+                {
+
+                    for (int j = 0; j < English.Length; j++)
+                    {
+                        if (words[i].Equals(English[j]))
+                        {
+                            wordsFound++;
+                        }
+                    }
+                }
+                // Find the percentage of words found in dictionary
+                percentage = (wordsFound / words.Length) * 100;
+                Console.WriteLine(wordsFound + " out of " + words.Length +
+                    " words found in English dictionary. It is " + percentage + "% English");
+            }
+
+            // If not all character belong to English alphabet display message
+            else
+            {
+                Console.WriteLine("Not all characters belong to the English alphabet");
+            }
+        }
+    }
+}
+
--- FastBrain/FastBrain/Properties/AssemblyInfo.cs
+++ FastBrain/FastBrain/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DemoLanguageDetect")]
+[assembly: AssemblyTitle("FastBrain")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DemoLanguageDetect")]
+[assembly: AssemblyProduct("FastBrain")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5e0e9be9-0b7b-4fce-9325-7545f90f491b")]
+[assembly: Guid("bbb2abb5-6579-45a8-855e-b23b7cc8a5ec")]
 
 // Version information for an assembly consists of the following four values:
 //
