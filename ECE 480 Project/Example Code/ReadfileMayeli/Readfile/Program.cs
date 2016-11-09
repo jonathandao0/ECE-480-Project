da936ce3a5d135bdf7210cd8bf8222d9fa80ab0d
Mayeli <projectcalpoly@gmail.com>
2016-11-08 20:48:58 -0800

    This is my code which it reads/write to a textfile.

---
+++ ECE 480 Project/Example Code/ReadfileMayeli/Readfile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // StreamRteader to read file and closing it
+                using (StreamReader sr = new StreamReader("c:/learningtext.txt"))
+                {
+                    string line;
+
+                    // Read and display lines from the file until the end of the file is reached. 
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                // Message displaying if something go wrong
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
