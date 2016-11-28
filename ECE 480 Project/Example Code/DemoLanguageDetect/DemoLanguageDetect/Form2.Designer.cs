dacba9b66bdb2859ae91999e7f3e5d7fbb4b7290
diegoilopez <catusdesiano@gmail.com>
2016-11-27 20:00:55 -0800

    Diego Ignacio Changes
    I included a variable TIME which holds the time in miliseconds.
    I also put mos of the main function code into a function which has the
    possibility of running asyncronously with another function, although I
    realized that such a thing might not be necessary. Hopefully it's
    satisfying. If not, specifically tell me what is wrong and what is the
    path I should take instead.

--- ECE 480 Project/Example Code/DemoLanguageDetect/DemoLanguageDetect/Form2.Designer.cs
+++ ECE 480 Project/Example Code/DemoLanguageDetect/DemoLanguageDetect/Form2.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DemoLanguageDetect
 {
-    partial class Form2
+    public partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -39,18 +39,21 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 175);
+            this.label1.Location = new System.Drawing.Point(14, 142);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Probability:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 203);
+            this.label2.Location = new System.Drawing.Point(14, 165);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.Size = new System.Drawing.Size(40, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Result:";
             // 
@@ -59,9 +62,10 @@
             this.probOutput.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.probOutput.Cursor = System.Windows.Forms.Cursors.No;
             this.probOutput.Enabled = false;
-            this.probOutput.Location = new System.Drawing.Point(103, 170);
+            this.probOutput.Location = new System.Drawing.Point(77, 138);
+            this.probOutput.Margin = new System.Windows.Forms.Padding(2);
             this.probOutput.Name = "probOutput";
-            this.probOutput.Size = new System.Drawing.Size(123, 22);
+            this.probOutput.Size = new System.Drawing.Size(93, 20);
             this.probOutput.TabIndex = 6;
             // 
             // result
@@ -69,42 +73,48 @@
             this.result.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.result.Enabled = false;
             this.result.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.result.Location = new System.Drawing.Point(103, 198);
+            this.result.Location = new System.Drawing.Point(77, 161);
+            this.result.Margin = new System.Windows.Forms.Padding(2);
             this.result.Name = "result";
-            this.result.Size = new System.Drawing.Size(123, 22);
+            this.result.Size = new System.Drawing.Size(93, 20);
             this.result.TabIndex = 7;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 21);
+            this.label3.Location = new System.Drawing.Point(14, 17);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.Size = new System.Drawing.Size(64, 13);
             this.label3.TabIndex = 8;
             this.label3.Text = "String Input:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // inputText
             // 
             this.inputText.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.inputText.Enabled = false;
-            this.inputText.Location = new System.Drawing.Point(103, 21);
+            this.inputText.Location = new System.Drawing.Point(77, 17);
+            this.inputText.Margin = new System.Windows.Forms.Padding(2);
             this.inputText.Multiline = true;
             this.inputText.Name = "inputText";
-            this.inputText.Size = new System.Drawing.Size(256, 140);
+            this.inputText.Size = new System.Drawing.Size(193, 114);
             this.inputText.TabIndex = 9;
+            this.inputText.TextChanged += new System.EventHandler(this.inputText_TextChanged);
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScrollMargin = new System.Drawing.Size(2, 2);
-            this.ClientSize = new System.Drawing.Size(365, 262);
+            this.ClientSize = new System.Drawing.Size(274, 213);
             this.Controls.Add(this.inputText);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.result);
             this.Controls.Add(this.probOutput);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form2";
             this.Text = "Result";
             this.Load += new System.EventHandler(this.Form2_Load);
@@ -115,7 +125,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox probOutput;
         private System.Windows.Forms.TextBox result;
--- ECE 480 Project/MainProgramCode.cs
+++ ECE 480 Project/MainProgramCode.cs
@@ -3,38 +3,47 @@ using System;
 using System.Collections.Generic;
 //using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.IO;
+using System.Diagnostics;
 
 namespace ECE_480_Project
 {
     class MainProgramCode
     {
-        //static Language[] langs = new Language[3];
-        //static ILangProcess[] processes = new ILangProcess[3];
-
+        static Task task;///Diego->I added this
+        static Language[] langs = new Language[3];            //Diego->I uncomented this
+        static ILangProcess[] processes = new ILangProcess[3]; //Diego->I uncomented this
+        static string languageCode;///Diego->I added this global variable previously found in the main function.
         const string knownLanguagesFile = @"Languages\known_languages.txt";
         public static Language[] MainProgram(string stringInput, out string ID, out double probability)
         {
+            /*Diego Lopez included this
             Language[] langs = new Language[3];
             ILangProcess[] processes = new ILangProcess[3];
             // Fast Brain Process
-            var learner = new LanguageLearner();
-            var knownLanguages = learner.Remember(knownLanguagesFile);
-            var detector = new LanguageDetector(knownLanguages);
-            int score;
-            var languageCode = detector.Detect(stringInput, out score);
 
-            // each FBP should inherant ILangProcess interface
-            switch(languageCode)
-            {
-                case "en": 
-                    processes[0] = new EnglishFBP(stringInput,score);
-                    break;
-                default:
-                    break;
+            */
+            ////Diego Lopez Inlcuded This
 
-            }
+            /*  var learner = new LanguageLearner();                            ////Nacho included commented this mothefucking paragraph
+              var knownLanguages = learner.Remember(knownLanguagesFile);
+              var detector = new LanguageDetector(knownLanguages);
+              int score;
+              var languageCode = detector.Detect(stringInput, out score);
 
+              // each FBP should inherant ILangProcess interface
+              switch(languageCode)
+              {
+                  case "en": 
+                      processes[0] = new EnglishFBP(stringInput,score);
+                      break;
+                  default:
+                      break;
+
+              }
+              */
+            ////Nacho included commented this mothefucking paragraph
             //processes[0] = new EnglishFBP(stringInput);
             //processes[1] = new EnglishFBP(stringInput);
             //processes[2] = new EnglishFBP(stringInput);
@@ -42,6 +51,20 @@ namespace ECE_480_Project
             // Diego: Thread all processes here for FBP & add runtime start/stops
 
             // return results here
+
+
+            ///Diego added this part of the code starting here
+          Stopwatch stopwatch = new Stopwatch();
+          stopwatch.Start();
+          CallThreadingProcess(stringInput);
+          Task.WaitAll(task);
+          stopwatch.Stop();
+          TimeSpan ts = stopwatch.Elapsed;
+          var second = ts.Seconds;
+          var milisecond = ts.Milliseconds;
+          var TIME = second + milisecond * 1000;//Time in seconds with a milisecond precission. 
+            ///Diego added this part of the code ending here
+           
             langs[0] = processes[0].Lang;
             int count = 0;
             foreach (var lang in langs)
@@ -54,10 +77,59 @@ namespace ECE_480_Project
                     // Slow Brain processes
                 }
             }
+
             ID = languageCode;
-            probability = langs[0].probability;
+            probability =langs[0].probability;
             return langs;
         }
+
+        private static void ThreadingProcess(string stringInput)
+        {                                                   
+            /////This is kevin's code put into a function. If I am not mistaken, this function is identifying the language in the string.
+            ///There are some other operations which I decided to include into the function
+            ///I also converted into global variables some variables that previously were local(
+            var learner = new LanguageLearner();
+            var knownLanguages = learner.Remember(knownLanguagesFile);
+            var detector = new LanguageDetector(knownLanguages);
+            int score;
+            languageCode = detector.Detect(stringInput, out score);
+            switch (languageCode)
+            {
+                case "en":
+                    processes[0] = new EnglishFBP(stringInput, score);
+                    break;
+                default:
+                    break;
+
+            }
+        }
+        ///The following two functions are just to make the above function work asyncrhonoulsy. It would be of some use if we were 
+        ///working with several processes at the same time. However, it seems that we are only working with one: identifying one language.
+         private static Task ThreadingProcessAsync(string stringInput)
+        {
+            task = Task.Factory.StartNew(() => ThreadingProcess(stringInput));
+            return task; ;
+        }
+
+         private static async void CallThreadingProcess(string stringInput)
+         {
+             await ThreadingProcessAsync(stringInput);
+         }
+
+
+
+
+
+        /*private static Task waitingAsync(int id)
+        {
+            task[id] = Task.Factory.StartNew(() => waiting(id));
+            return task[id];
+        }
+        private static async void Callwait(int id)
+        {
+            await waitingAsync(id);
+        }
+    }*/
     }
 }
 
