7a39494948dcefeed5ca270a42f8f29bb207f1ce
diegoilopez <catusdesiano@gmail.com>
2016-11-28 21:21:34 -0800

    This time I included threading
    I changed some comments where I typed wrong words(miliseconds instead of
    seconds, etc.)
    I also included what I believe is what Jonathan wanted.

--- ECE 480 Project/MainProgramCode.cs
+++ ECE 480 Project/MainProgramCode.cs
@@ -20,12 +20,10 @@ namespace ECE_480_Project
          
         public static Language MainProgram(string stringInput)//, out string ID, out double probability)
         {
-           Stopwatch stopwatch = new Stopwatch();///Diebo->I had to include this because Stopwatch as a static global class was not measuring the right time.
+          // stopwatch object use to measure the Fast Brain process running time
+           Stopwatch stopwatch = new Stopwatch();///Diego->I had to include this because Stopwatch as a static global class was not measuring the right time.
             Language lang = new Language();
-           // ILangProcess[] processes = new ILangProcess[3];
-
-            // Fast Brain Processes
-            stopwatch.Start();
+            stopwatch.Start(); 
             var learner = new LanguageLearner();
             var knownLanguages = learner.Remember(knownLanguagesFile);
             var detector = new LanguageDetector(knownLanguages);
@@ -39,13 +37,13 @@ namespace ECE_480_Project
             switch(languageCode)
             {
                 case "en":
-                 //   lang = fastBrainProcessEnglish(lang);
+                   lang = fastBrainProcessEnglish(lang, stopwatch);
                     break;
                 case "es":
-                    //lang = fastBrainProcessSpanish(lang);
+                    lang = fastBrainProcessSpanish(lang,stopwatch);
                     break;
                 case "ru":
-                    //lang = fastBrainProcessRussian(lang);
+                    lang = fastBrainProcessRussian(lang, stopwatch);
                     break;
                 default:
                     // Launch Error Window? Loc: I handle this by outputing result = "Undefined" when probability = 0%
@@ -55,15 +53,18 @@ namespace ECE_480_Project
           
             if (lang.probability < 15)///I changed it to 15 so I was able to easily test the slow BP. Diego
             {
-                Thread.Sleep(2999);//This line right here should be eliminated. I'm just including a 10s delay to simulte the Slow BP. Diego
+                //stopwatch2 object used to measure the Slow Brain process running time 
+                Stopwatch stopwatch2 = new Stopwatch();
+                stopwatch2.Start();
+                Thread.Sleep(2999);//This line right here should be eliminated. I'm just including a 2999s delay to simulte the Slow BP. Diego
                 
                 // Slow Brain processes
                 //langs =  SlowBrainProcess.SlowBrainProcess(langs);
 
                 //The slow brain process or function from an object should be put above this code.Diego
                 //Measuring SlowBrainProcess run time, in case slow brain  is used. 
-                stopwatch.Stop();
-                TimeSpan ts = stopwatch.Elapsed;
+                stopwatch2.Stop();
+                TimeSpan ts = stopwatch2.Elapsed;
 
                 // TIME should be a double
                 double second = ts.Seconds;
@@ -77,32 +78,13 @@ namespace ECE_480_Project
                 lang.slowBrainRuntime = TIME;
             }
 
-            // This else statement will measure the time in case the slow brain is not used
-            else
-            {
-               
-            stopwatch.Stop();
-            TimeSpan ts = stopwatch.Elapsed;
-
-            // TIME should be a double
-            double second = ts.Seconds;
-            double milisecond = ts.Milliseconds;
-
-            //Time in miliseconds. I decided not to do in milliseconds because when dividing 
-            //milisecond by 1000 then the result was only giving zeroes.
-            double TIME = second * 1000 + milisecond;
-            //fastBrainRuntime = TIME and slowBrainRuntime=0;
-            lang.fastBrainRuntime = TIME;
-            lang.slowBrainRuntime = 0;
-            }
-          
             return lang;
         }
 
         // Diego: Repeat function for Spanish and Russian
-           static Language fastBrainProcessEnglish(Language eng)
+        static Language fastBrainProcessEnglish(Language eng, Stopwatch stopwatch)
             {
-                Stopwatch stopwatch = new Stopwatch();
+               
                 Task<Language> task;
                 Language lang;
                 task = Task<Language>.Factory.StartNew(() =>
@@ -117,16 +99,17 @@ namespace ECE_480_Project
                 // TIME should be a double
                 var second = ts.Seconds;
                 var milisecond = ts.Milliseconds;
-                //Time in miliseconds. I decided not to do in milliseconds because when dividing 
+                //Time in miliseconds. I decided not to do in seconds because when dividing 
                 //milisecond by 1000 then the result was only giving zeroes.
                 double TIME = second * 1000 + milisecond; 
                 lang.fastBrainRuntime = TIME;
+                lang.slowBrainRuntime = 0; //slowBrainRuntime=0 unless it goes into the if statement, in which case the  if (lang.probability < 15) statment will deal with it
                 return lang;
             }
 
-            static Language fastBrainProcessRussian(Language eng)
+           static Language fastBrainProcessRussian(Language eng, Stopwatch stopwatch)
             {
-                Stopwatch stopwatch = new Stopwatch();
+                
                 Task<Language> task;
                 Language lang;
                 task = Task<Language>.Factory.StartNew(() =>
@@ -141,21 +124,17 @@ namespace ECE_480_Project
                 // TIME should be a double
                 var second = ts.Seconds;
                 var milisecond = ts.Milliseconds;
-                //Time in miliseconds. I decided not to do in milliseconds because when dividing 
+                //Time in miliseconds. I decided not to do in seconds because when dividing 
                 //milisecond by 1000 then the result was only giving zeroes.
                 double TIME = second * 1000 + milisecond; 
                 lang.fastBrainRuntime = TIME;
+                lang.slowBrainRuntime = 0;//slowBrainRuntime=0 unless it goes into the if statement, in which case the  if (lang.probability < 15) statment will deal with it
                 return lang;
             }
-            //Hey jonathan, I know this demonstrates where our project could go into the future, by 
-            //applying different threads to look for the different languages simultaneously, but if I am
-            //not mistaken, I think this should by applied in the polyglot code, because the 
-            //main function does not use the FB processes. It only receives the information from the FB processes or the polyglot code.
-            //In that case, we could keep this function in case we have time to explain, but I'll just implement this code up above
-            
-            static Language fastBrainProcessSpanish(Language eng)
+           
+            static Language fastBrainProcessSpanish(Language eng,Stopwatch stopwatch)
             {
-                Stopwatch stopwatch = new Stopwatch();
+               
                 Task<Language> task;
                 Language lang;
                 task = Task<Language>.Factory.StartNew(() =>
@@ -171,68 +150,14 @@ namespace ECE_480_Project
                 var second = ts.Seconds;
                 var milisecond = ts.Milliseconds;
 
-                //Time in miliseconds. I decided not to do in milliseconds because when dividing 
+                //Time in miliseconds. I decided not to do in seconds because when dividing 
                 //milisecond by 1000 then the result was only giving zeroes.
                 double TIME = second * 1000 + milisecond; 
                 lang.fastBrainRuntime = TIME;
+                lang.slowBrainRuntime = 0;//slowBrainRuntime=0 unless it goes into the if statement, in which case the  if (lang.probability < 15) statment will deal with it
                 return lang;
             }
-            //fastBrainProcessSpanish
-
-            //fastBrainProcessRussian
-
-            /*
-            private static void ThreadingProcess(string stringInput)
-            {                                                   
-                /////This is kevin's code put into a function. If I am not mistaken, this function is identifying the language in the string.
-                ///There are some other operations which I decided to include into the function
-                ///I also converted into global variables some variables that previously were local(
-                var learner = new LanguageLearner();
-                var knownLanguages = learner.Remember(knownLanguagesFile);
-                var detector = new LanguageDetector(knownLanguages);
-                int score;
-                languageCode = detector.Detect(stringInput, out score);
-                switch (languageCode)
-                {
-                    case "en":
-                        processes[0] = new EnglishFBP(stringInput, score);
-                        break;
-                    default:
-                        break;
-
-                }
-            }
-            */
-
-
-        /*
-        ///The following two functions are just to make the above function work asyncrhonoulsy. It would be of some use if we were 
-        ///working with several processes at the same time. However, it seems that we are only working with one: identifying one language.
-        private static Task ThreadingProcessAsync(string stringInput)
-        {
-            var task = Task.Factory.StartNew(() => ThreadingProcess(stringInput));
-            return task; ;
-        }
-
-        private static async void CallThreadingProcess(string stringInput)
-        {
-            await ThreadingProcessAsync(stringInput);
-        }
-
-
-
-
-
-        /*private static Task waitingAsync(int id)
-        {
-            task[id] = Task.Factory.StartNew(() => waiting(id));
-            return task[id];
-        }
-        private static async void Callwait(int id)
-        {
-            await waitingAsync(id);
-        }
-    }*/
+        
     }
 }
 
